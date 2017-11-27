59c562de1d3e764e083eda75c16a365a25bdfa61
rum-n <manev.ru@gmail.com>
2017-11-27 17:57:13 +0100

    homepage layout

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,22 @@
 //= require leaflet
 
+$(document).ready(function(){$("body").hide().fadeIn(2000);});
+
+$(function() {
+  
+  // contact form animations
+  $('#Btn2').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
 
